eecf4df9e7f249f90cafc20f15916057773ca36b
Sufian <simpleminded@tutamail.com>
2023-03-15 19:22:52 +0300

    add : Data to test website behavior
    Added 2 files
    Landing.Js : contains data intended to be used in the landing page (home), to display sections of content.
    
    Videos.js : contains data intended to be used for displaying videos.
    
    both files still need some work, but the data structure is good enough to be used

---
+++ src/Data/Landing.js
@@ -0,0 +1,63 @@
+module.exports =[
+    {
+        Type: "Parallax",
+        Content: {
+            Title: "Welcome",
+
+            Message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ut fugit, officia similique repellendus dolor explicabo placeat iusto quae deserunt vero nam dolores accusantium pariatur possimus sunt", //
+            
+            Image: null ,
+            Background: null ,
+        },
+        Upload_Date: 1666721514220, // Upload date
+        Modified_Date:1666721514220,// Last Modified date
+        __v: 0,
+    },
+    {
+        _id: "635826a257dac4b96f3d4846",
+        Type: "Content",
+        Content: {
+            Title: "Hello world",
+
+            Text: "Ab veniam impedit iste, mollitia et quae ipsum iure cupiditate repellat harum aliquid officia deleniti quos dolorem! Quos quod quam, corporis nemo molestiae quas unde sint iure facilis veritatis exercitationem!\nOdio vero et vitae iste suscipit! Nisi dolore sapiente a ratione porro optio sint maxime nobis. Ab tempora, voluptatum quas cum, aperiam hic sequi modi commodi atque odio similique odit!", //
+            
+        },
+        Upload_Date: 1666721514220, // Upload date
+        Modified_Date:1666721514220,// Last Modified date
+        __v: 0,
+    },
+    {
+        _id: "635826a257dac4b96f3d4846",
+        Type: "Parallax",
+        Content: {
+            Title: "Message Title",
+
+            Message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ut fugit, officia similique repellendus dolor explicabo placeat iusto quae deserunt vero nam dolores accusantium pariatur possimus sunt", //
+            
+            Image: null ,
+            Background: null ,
+        },
+        Upload_Date: 1666721514220, // Upload date
+        Modified_Date:1666721514220,// Last Modified date
+        __v: 0,
+    },
+    {
+        _id: "635826a257dac4b96f3d4846",
+        Type: "Content",
+        Content: {
+            Title: "Videos",
+
+            Text: "Ab veniam impedit iste, mollitia et quae ipsum iure cupiditate repellat harum aliquid officia deleniti quos dolorem! Quos quod qua.", //
+            
+            Tags :["Lorem","ipsum","dolor","sit","consectetur", "adipisicing","elit","Maiores","aspernatur"],
+
+            Videos:8,
+
+        },
+        Upload_Date: 1666721514220, // Upload date
+        Modified_Date:1666721514220,// Last Modified date
+        __v: 0,
+    },
+    
+]
+    
---
+++ src/Data/Videos.js
@@ -0,0 +1,65 @@
+module.exports =[
+    {
+        Tags: ["Lorem"],
+        Type: "video",
+        Content: {
+            Title: "Video",
+            
+            Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ut fugit, officia similique repellendus dolor explicabo placeat iusto quae deserunt vero nam dolores accusantium pariatur possimus sunt, doloribus quod sint!\nDucimus animi ex possimus quas omnis iste cupiditate reprehenderit tenetur. Iusto veniam reiciendis ducimus corporis nisi labore tempora at asperiores repellendus modi hic necessitatibus ipsum officia, enim temporibus laborum quod.",
+            
+            Video: null ,
+        },
+        Comments: 
+        [
+            {
+                id          : "63b08a7eaec37181be7b544a",
+                Type        : "comment",
+                Content     : {
+                    "Text"    : "Consectetur eveniet repudiandae nisi reprehenderit saepe! Assumenda, quae quisquam id sunt totam modi sint, illo necessitatibus itaque exercitationem autem deserunt explicabo officia numquam tempore esse soluta fuga nulla natus? Nesciunt.",
+                },
+                Upload_Date  : 1672514181334,
+                Modified_Date: 1672514181334,
+                
+                __v: 0
+            }
+
+        ],
+        Upload_Date: 1666721514220, // Upload date
+        Modified_Date:1666721514220,// Last Modified date
+        __v: 0,
+    },
+]
+    
+  
+
+
+//  Comment structure
+//   {
+//     "id"          : "",
+//     "Type"        : "comment",
+//     "Content"     : {
+//         "Text"    : "",
+//     },
+//     "Upload_Date"  : 1672514181334,
+//     "Modified_Date": 1672514181334,
+//     "__v": 0
+// }
+
+
+// Video structure
+// {
+//     "_id": "",
+//     "Comments": 
+//     [
+
+//     ],
+//     "Type": "video",
+//     "Content": {
+//         "Title": "",
+//         "Description": "",
+//         "Video": null ,
+//     },
+//     "Upload_Date": 1666721514220, // Upload date
+//     "Modified_Date":1666721514220,// Last Modified date
+//     "__v": 0,
+//   }
